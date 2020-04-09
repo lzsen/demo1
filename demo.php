5d6e29cda95e6787bad93d3285372d64c15c0079
lzsen <lzs0@163.com>
2020-04-09 23:30:12 +0800

    第一个版本

---
+++ demo.php
@@ -0,0 +1,5 @@
+<?php
+require './vendor/autoload.php';
+use xzljoec\demo1\Tools;
+
+Tools::hello();
---
+++ src/xzljoec/demo1/Tools.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace xzljoec\demo1;
+
+class Tools {
+
+
+	public function __construct(){
+
+	}
+
+	public static function hello(){
+		echo "Hello world";
+	}
+
+}
